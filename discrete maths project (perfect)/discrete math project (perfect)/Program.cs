641493356aea32d9892e7d3b240b6fa0ca69cb9e
ahmedwaelyousry <120680274+ahmedwaelyousry@users.noreply.github.com>
2022-12-24 13:09:50 +0200

    Add files via upload

---
+++ discrete maths project (perfect)/discrete math project (perfect)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discrete_maths_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, sum = 0;
+            Console.WriteLine("Enter the first number");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            n2 = int.Parse(Console.ReadLine());
+            for(int i = n1; i<=n2; i++)
+            {
+                for(int l = 1; l<=i/2; l++)
+                {
+                    if(i % l == 0)
+                    {
+                        sum += l;
+                    }
+                }
+                if(sum == i)
+                {
+                    Console.WriteLine(i + "is a perfect number");
+                }
+                sum = 0;
+            }
+        }
+    }
+}
---
+++ discrete maths project (prime)/discrete maths project (prime)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discrete_maths_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, prime = 0;
+            Console.WriteLine("Enter the first number");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            n2 = int.Parse(Console.ReadLine());
+            if (n1 == 1)
+            {
+                n1++;
+            }
+            if (n1 == 0)
+            {
+                n1 += 2;
+            }
+            for (int i = n1; i <= n2; i++)
+            {
+                for (int l = 2; l < i; l++)
+                {
+                    if (i % l == 0)
+                    {
+                        prime++;
+                    }
+
+                }
+                if (prime == 0)
+                {
+                    Console.WriteLine(i + " is a prime number");
+                }
+                else
+                {
+                    prime = 0;
+                }
+            }
+        }
+    }
+}
